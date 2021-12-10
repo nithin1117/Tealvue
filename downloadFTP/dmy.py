e653dbae470ed3367c04a8c428a3ccc4688f7d0b
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-12-10 22:15:56 +0530

    Create dmy.py

---
+++ downloadFTP/dmy.py
@@ -0,0 +1,22 @@
+import datetime
+import sys
+import calendar
+
+while True:
+    day = input('Enter date(dd/mm/yy) ')
+    try :
+        day = datetime.datetime.strptime(day, "%d/%m/%Y")
+        break
+    except ValueError:
+        print("Error: must be format dd/mm/yyyy ")
+        day = input('Enter date(dd/mm/yy)')
+        try:
+            day = datetime.datetime.strptime(day, "%d/%m/%Y")
+            break
+        except ValueError:
+            print("Invalid Input, Exited")
+            sys.exit()
+
+mon = calendar.month_abbr[int(day.month)]
+DMY = (f"{day.day}" + mon + f"{day.year}")
+print(DMY)
