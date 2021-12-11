7d949fd5c84ec6b615b07c45a13e987d3279c72c
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-12-10 22:06:35 -0800

    Update FTP.py
    download and extrect ftp

--- downloadFTP/FTP.py
+++ downloadFTP/FTP.py
@@ -1,9 +1,15 @@
 from ftplib import FTP
+from zipfile import ZipFile
 from datetime import date
 
+import gzip
+import shutil
+
+
 today = date.today()
-mon = today.strftime("%b").upper()
-DMY = today.strftime("%d"+ mon +"%Y")
+mon = today.strftime("%b")
+DMY = today.strftime("%d" + mon + "%Y")
+
 
 
 ftp = FTP('ftp.connect2nse.com')
@@ -22,16 +28,47 @@ fileName5 = "security.gz"
 fileName6 = "spd_contract.gz"
 
 #dir = input()
-#change directory according to your preferences
-dir = "/home/nithin/Documents"
+dir = "/home/nithin/Documents/sample/new/"
 
-ftp.retrbinary("RETR " + fileName1, open(dir + fileName1, 'wb').write)
-ftp.retrbinary("RETR " + fileName2, open(dir + fileName2, 'wb').write)
-ftp.retrbinary("RETR " + fileName3, open(dir + fileName3, 'wb').write)
-ftp.retrbinary("RETR " + fileName4, open(dir + fileName4, 'wb').write)
+ftp.retrbinary("RETR " + fileName1, open(dir + fileName1[:11], 'wb').write)
+ftp.retrbinary("RETR " + fileName2, open(dir + fileName2[:18], 'wb').write)
+ftp.retrbinary("RETR " + fileName3, open(dir + fileName3[:15], 'wb').write)
+ftp.retrbinary("RETR " + fileName4, open(dir + fileName4[:14], 'wb').write)
 ftp.retrbinary("RETR " + fileName5, open(dir + fileName5, 'wb').write)
 ftp.retrbinary("RETR " + fileName6, open(dir + fileName6, 'wb').write)
 
 
 ftp.close()
 print("Downloaded")
+
+with open('/home/nithin/Documents/sample/new/contract.gz', 'rb') as f_in:
+    with gzip.open('/home/nithin/Documents/sample/new/extracted/contract' + DMY + '.txt', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+ 
+
+with open('/home/nithin/Documents/sample/new/nnf_participant.gz', 'rb') as f_in:
+    with gzip.open('/home/nithin/Documents/sample/new/extracted/nnf_participant' + DMY + '.txt', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+with open('/home/nithin/Documents/sample/new/nnf_security.gz', 'rb') as f_in:
+    with gzip.open('/home/nithin/Documents/sample/new/extracted/nnf_security' + DMY + '.txt', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+with open('/home/nithin/Documents/sample/new/participant.gz', 'rb') as f_in:
+    with gzip.open('/home/nithin/Documents/sample/new/extracted/participant' + DMY + '.txt', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+ 
+
+with open('/home/nithin/Documents/sample/new/security.gz', 'rb') as f_in:
+    with gzip.open('/home/nithin/Documents/sample/new/extracted/security' + DMY + '.txt', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+        
+with open('/home/nithin/Documents/sample/new/spd_contract.gz', 'rb') as f_in:
+    with gzip.open('/home/nithin/Documents/sample/new/extracted/spd_contract' + DMY + '.txt', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+        
+
+
+print("Extracted")
+
+
