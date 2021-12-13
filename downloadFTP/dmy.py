aab0bb71b2edba7fc7651a3ff925160dd722ae3e
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-12-13 16:26:22 +0530

    Update dmy.py

--- downloadFTP/dmy.py
+++ downloadFTP/dmy.py
@@ -1,36 +1,40 @@
+import argparse
 import datetime
 import sys
 import calendar
 
-while True:
-    day = input('Enter date(dd/mm/yy):')
-    try :
-        day = datetime.datetime.strptime(day, "%d/%m/%Y")
-        break
-    except ValueError:
 
-        day = input('Error: Try in the format dd/mm/yyyy:')
-        try:
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--date", help="name of the user")
+args = vars(ap.parse_args())
+dmy = args["date"]
+
+def AutoDate():
+    from datetime import date
+    today = date.today()
+    mon = today.strftime("%b")
+    dmy = today.strftime("%d" + mon + "%Y")
+    return dmy
+
+
+if dmy == None:
+    dmy= AutoDate()
+    
+else:
+    while True:
+        day = dmy
+        try :
             day = datetime.datetime.strptime(day, "%d/%m/%Y")
             break
         except ValueError:
-            print("Invalid Input, Exited")
+            print('Error: Invalid Date Format, Try DD/MM/YYY')
             sys.exit()
 
-mon = calendar.month_abbr[int(day.month)]
-DMY = (f"{day.day}" + mon + f"{day.year}")
-print(DMY)
+    mon = calendar.month_abbr[int(day.month)]
+    DMY = (f"{day.day}" + mon + f"{day.year}")
 
 
 
 
-# #today's date
-# from datetime import date
-
-# today = date.today()
-# mon = today.strftime("%b")
-
-# DMY = today.strftime("%d" + mon + "%Y")
-
-# print(DMY)
-
+val = "val" + dmy
+print(val)
