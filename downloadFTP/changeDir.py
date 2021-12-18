da80e50e208845d584e9901749323d9fa33a612d
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-12-18 12:55:55 +0530

    Delete changeDir.py

--- downloadFTP/changeDir.py
+++
@@ -1,9 +0,0 @@
-import shutil
-
-original = r'original path where the directory is currently stored\directory name'
-target = r'target path where the directory will be moved\directory name'
-
-# original = input()
-# target = input()
-
-shutil.move(original,target)
