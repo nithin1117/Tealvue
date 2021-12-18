895e06a3fb1ec749b9755c847bb1388df51a8b9d
Nithin K <77234820+nithin1117@users.noreply.github.com>
2021-12-18 12:56:58 +0530

    Update FTP.py

--- downloadFTP/FTP.py
+++ downloadFTP/FTP.py
@@ -1,43 +1,50 @@
 from ftplib import FTP
 import gzip
 import shutil
-    
-    
-def inputDate():
-    import datetime
-    import calendar
-    while True:
-        day = input('Enter date[DD/MM/YYYY]:')
-        try:
-            day = datetime.datetime.strptime(day, "%d/%m/%Y")
-            break
-        except ValueError:
-            print('Error: Accepted format DD/MM/YYYY')
+import argparse
+import os
 
-    mon = calendar.month_abbr[int(day.month)]
-    DMY = (f"{day.day}" + mon + f"{day.year}")
-    return DMY
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--date", help="name of the user")
+args = vars(ap.parse_args())
+dmy = args["date"]
 
 
 def AutoDate():
     from datetime import date
     today = date.today()
     mon = today.strftime("%b")
-    DMY = today.strftime("%d" + mon + "%Y")
-    return DMY
+    dmy = today.strftime("%d" + mon + "%Y")
+    return dmy
 
 
-'''AutoDate function detect date automatically where inputDate has to be entered manually'''
-# DMY = AutoDate()
-DMY = inputDate()
+if dmy == None:
+    dmy = AutoDate()
 
-if DMY[1].isalpha():
-    DMY = DMY.zfill(9)
+else:
+    import datetime
+    import calendar
+    import sys
+
+    while True:
+        day = dmy
+        try:
+            day = datetime.datetime.strptime(day, "%d/%m/%Y")
+            break
+        except ValueError:
+            print('Error: Invalid Date Format, Try DD/MM/YYY')
+            sys.exit()
+
+    mon = calendar.month_abbr[int(day.month)]
+    dmy = (f"{day.day}" + mon + f"{day.year}")
+
+if dmy[1].isalpha():
+    dmy = dmy.zfill(9)
 else:
     pass
 
 
-def ftpfile(DMY):
+def ftpfile(dmy):
     ftp = FTP('ftp.connect2nse.com')
     ftp.login('FTPGUEST', 'FTPGUEST')
 
@@ -45,67 +52,60 @@ def ftpfile(DMY):
     ftp.cwd(path)
 
     try:
-        dir1 = input("enter path with / in front and back:")
-        #dir1 = "/home/nithin/Documents/sample/new/check/"
 
-        fName1 = "contract.gz_" + DMY
-        fName2 = "nnf_participant.gz_" + DMY
-        fName3 = "nnf_security.gz_" + DMY
-        fName4 = "participant.gz_" + DMY
-        fName5 = "security.gz"
-        fName6 = "spd_contract.gz"
+        key = 'FTPFILES'
+        dir1 = os.getenv(key) + "/"
 
-        pdir1 = dir1 + fName1
-        pdir2 = dir1 + fName2
-        pdir3 = dir1 + fName3
-        pdir4 = dir1 + fName4
-        pdir5 = dir1 + fName5
-        pdir6 = dir1 + fName6
-
-
-        ftp.retrbinary("RETR " + fName1, open(pdir1, 'wb').write)
-        ftp.retrbinary("RETR " + fName2, open(pdir2, 'wb').write)
-        ftp.retrbinary("RETR " + fName3, open(pdir3, 'wb').write)
-        ftp.retrbinary("RETR " + fName4, open(pdir4, 'wb').write)
-        ftp.retrbinary("RETR " + fName5, open(pdir5, 'wb').write)
-        ftp.retrbinary("RETR " + fName6, open(pdir6, 'wb').write)
-
-        ftp.close()
-        print("Downloaded")
+        # dir1 = "/home/nithin/Documents/sample/new/check/"
         try:
-            with open(pdir1, 'rb') as f_in:
-                with gzip.open(dir1 + fName1 + '.txt', 'wb') as f_out:
+            fName1 = "contract.gz_" + dmy
+            fName2 = "nnf_participant.gz_" + dmy
+            fName3 = "nnf_security.gz_" + dmy
+            fName4 = "participant.gz_" + dmy
+            fName5 = "security.gz"
+            fName6 = "spd_contract.gz"
+
+            ftp.retrbinary("RETR " + fName1, open(dir1 + fName1, 'wb').write)
+            ftp.retrbinary("RETR " + fName2, open(dir1 + fName2, 'wb').write)
+            ftp.retrbinary("RETR " + fName3, open(dir1 + fName3, 'wb').write)
+            ftp.retrbinary("RETR " + fName4, open(dir1 + fName4, 'wb').write)
+            ftp.retrbinary("RETR " + fName5, open(dir1 + fName5, 'wb').write)
+            ftp.retrbinary("RETR " + fName6, open(dir1 + fName6, 'wb').write)
+
+            ftp.close()
+            with open(dir1 + fName1, 'rb') as f_in:
+                with gzip.open(dir1 + fName1[:-13] + dmy + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
-    
-            with open(pdir2, 'rb') as f_in:
-                with gzip.open(dir1 +fName2 + '.txt', 'wb') as f_out:
+
+            with open(dir1 + fName2, 'rb') as f_in:
+                with gzip.open(dir1 + fName2[:-13] + dmy + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
-    
-            with open(pdir3, 'rb') as f_in:
-                with gzip.open(dir1 +fName3 + '.txt', 'wb') as f_out:
+
+            with open(dir1 + fName3, 'rb') as f_in:
+                with gzip.open(dir1 + fName3[:-13] + dmy + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
-    
-    
-            with open(pdir4, 'rb') as f_in:
-                with gzip.open(dir1 +fName4 + '.txt', 'wb') as f_out:
+
+            with open(dir1 + fName4, 'rb') as f_in:
+                with gzip.open(dir1 + fName4[:-13] + dmy + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
-    
-    
-            with open(pdir5, 'rb') as f_in:
-                with gzip.open(dir1 +fName5 + '.txt', 'wb') as f_out:
+
+            with open(dir1 + fName5, 'rb') as f_in:
+                with gzip.open(dir1 + fName5[:-4] + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
-    
-    
-            with open(pdir6, 'rb') as f_in:
-                with gzip.open(dir1 +fName6 + '.txt', 'wb') as f_out:
+
+            with open(dir1 + fName6, 'rb') as f_in:
+                with gzip.open(dir1 + fName6[:-4] + '.txt', 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
-    
-            print("Extracted!!!...")
+
+            print("Downloaded and Extracted!!!...")
+
         except:
-            print("zip file path can't found")
-
+            print("Error: File not found in FTP")
+            return "error"
     except:
-        print("File not Found in FTP")
+        print("Error: path location had not prefered")
+        print("run 'sudo gedit /etc/environment' and set 'FTPFILES = /*desired directory*/'")
+        sys.exit()
 
 
-ftpfile(DMY)
+ftpfile(dmy)
